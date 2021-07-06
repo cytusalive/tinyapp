17a03755d94fc59b32cd81d98f63a95c90d9859a
cytusalive <cytusomega@gmail.com>
2021-07-06 22:08:04 +0000

    request url and random url generator

--- express_server.js
+++ express_server.js
@@ -1,14 +1,31 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+  let string = '';
+  const digits = 6;
+  for (let i = 0; i < digits; i++) {
+    string += chars[parseInt(chars.length * Math.random())];
+  }
+  return string;
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -25,6 +42,10 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
